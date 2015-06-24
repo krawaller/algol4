8a3463d492f465fefc93ca82a0ec9380217eef77
KungD <david@krawaller.se>
2015-06-24 20:53:15 +0200

    prepare function rework

---
+++ src/prepare.js
@@ -0,0 +1,104 @@
+(function(){
+var _ = (typeof require !== "undefined" ? require("./lodashmixins") : window._);
+var I = (typeof require !== "undefined" ? require("./immutableextensions.js") : window.Immutable);
+function augmentWithPrepareFunctions(Algol){
+
+
+// €€€€€€€€€€€€€€€€€€€€€€€€€€€ P R E P A R E   F U N C T I O N S €€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€*/
+
+
+/*
+Called at the beginning of every step.
+*/
+Algol.prepareUnitLayersFromData = function(unitsdata,plr){
+	var to = I.Map().set(0,"NEUTRALS").set(undefined,"NEUTRALS").set(plr,"MYUNITS");
+	return unitsdata.reduce(function(map,unit){
+		var pos = unit.get("POS");
+		return unit.get("STATUS") === "DEAD" ? I.pushIn(map,["DEADUNITS",pos],unit) : 
+			I.pushIn(I.pushIn(map,[(to.get(unit.get("PLR"))||"OPPUNITS"),pos],unit),["UNITS",pos],unit);
+	},blueprint,this);
+};
+var blueprint = I.fromJS({UNITS:{},DEADUNITS:{},MYUNITS:{},OPPUNITS:{},NEUTRALS:{}});
+
+/*
+Used in prepareState
+*/
+Algol.prepareInitialUnitDataFromSetup = function(setup){
+	return I.Map(Algol.prepareEntitiesFromList(setup).reduce(function(map,unit,n){
+		return map.set("unit"+(n+1),unit.set("id","unit"+(n+1)));
+	},I.Map(),this));
+};
+
+/*
+Helper function used only in prepareEntitiesFromList
+*/
+Algol.addEntitiesFromDef = function(coll,def){
+	if (I.List.isList(def)){ // [ALL,<list>,<blueprint>]
+		return def.get(1).reduce(function(mem,pos){
+			return mem.push(def.get(2).set("POS",pos));
+		},coll);
+	} else {
+		return coll.push(def);
+	}
+}
+
+/*
+Used in prepareState 
+*/
+Algol.prepareEntitiesFromList = function(deflist){
+	return deflist.reduce(Algol.addEntitiesFromDef,I.List());
+}
+
+/*
+Used in prepareState
+*/
+Algol.prepareConnectionsFromBoardDef = function(boarddef){
+	var height = boarddef.get("height"), width = boarddef.get("width");
+	return I.Range(1,width+1).reduce(function(mem,x){
+		return I.Range(1,height+1).reduce(function(mem,y){
+			return mem.set(y*1000+x,I.Map({x:x,y:y,nextto:_.reduce([[0,-1],[1,-1],[1,0],[1,1],[0,1],[-1,1],[-1,0],[-1,-1]],function(map,mods,n){
+				var newx = x+mods[0], newy = y+mods[1];
+				return newx>0 && newx<=width && newy>0 && newy<=height ? map.set(n+1,newy*1000+newx) : map;
+			},I.Map())}));
+		},mem);
+	},I.Map());
+};
+
+/*
+Used in prepareState
+*/
+Algol.prepareBoardLayersFromBoardDef = function(boarddef){
+	var height = boarddef.get("height"), width = boarddef.get("width");
+	return I.Range(1,width+1).reduce(function(mem,x){
+		return I.Range(1,height+1).reduce(function(mem,y){
+			var pos = y*1000+x, clr = ["light","dark"][(x+(y%2))%2], obj = I.Map({
+				X: x, Y: y, POS: pos, COLOUR: clr
+			})
+			return mem.setIn(["BOARD",pos],I.List([obj])).setIn([(clr==="light"?"LIGHT":"DARK"),pos],I.List([obj]));
+		},mem);
+	},I.Map());
+};
+
+/*
+The master function to set up a new state. Called once.
+*/
+Algol.prepareState = function(gamedef,players){
+	return I.fromJS({
+		connections: Algol.prepareConnectionsFromBoardDef(gamedef.get("board")),
+		data: I.Map().set("units",Algol.prepareInitialUnitDataFromSetup(gamedef.get("setup"))),
+		baselayers: (gamedef.get("terrain")||I.Map()).reduce(function(mem,layerdef,layername){
+			return mem.set(layername,Algol.prepareEntitiesFromList(layerdef));
+		},Algol.prepareBoardLayersFromBoardDef(gamedef.get("board")))
+	});
+};
+
+
+// €€€€€€€€€€€€€€€€€€€€€€€€€ E X P O R T €€€€€€€€€€€€€€€€€€€€€€€€€
+
+} // end augmentWithPrepareFunctions
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = augmentWithPrepareFunctions;
+} else {
+    window.augmentWithPrepareFunctions = augmentWithPrepareFunctions;
+}})();
---
+++ test/specs/prepare.spec.js
@@ -0,0 +1,151 @@
+/* jshint jasmine: true */
+
+var sinon,jasmineSinon,Algol,_,I,tester;
+if (typeof require === 'function' && typeof module === 'object') {
+	sinon = require('sinon');
+	jasmineSinon = require('jasmine-sinon');
+	Algol = require("../../src/");
+	_ = require("../../src/lodashmixins");
+	I = require("../../src/immutableextensions");
+	tester = require("../tester");
+} else {
+	I = window.Immutable;
+	_ = window._;
+	sinon = window.sinon;
+	tester = window.tester;
+}
+
+tester("The prepare methods",Algol,{
+	"prepareUnitLayersFromData(unitsdata,plr)": {
+		"for normal call": {
+			unitsdata: {
+				id1: {POS:"pos1",PLR:1},
+				id2: {POS:"pos2",PLR:2},
+				id3: {POS:"pos2",STATUS:"DEAD",PLR:1},
+				id4: {POS:"pos2",PLR:0}
+			},
+			plr: 1,
+			expected: {
+				DEADUNITS:{pos2:[{POS:"pos2",STATUS:"DEAD",PLR:1}]},
+				UNITS:{pos1:[{POS:"pos1",PLR:1}],pos2:[{POS:"pos2",PLR:2},{POS:"pos2",PLR:0}]},
+				MYUNITS:{pos1:[{POS:"pos1",PLR:1}]},
+				OPPUNITS:{pos2:[{POS:"pos2",PLR:2}]},
+				NEUTRALS:{pos2:[{POS:"pos2",PLR:0}]}
+			}
+		}
+	},
+	"prepareInitialUnitDataFromSetup(setup)": {
+		"for normal call": {
+			setup: "SETUP",
+			context: {
+				prepareEntitiesFromList: {
+					returns: [{foo:"bar"},{baz:"bin"}],
+					expectedargs: [["SETUP"]],
+				}
+			},
+			expected: {unit1: {foo:"bar",id:"unit1"}, unit2: {baz:"bin",id:"unit2"}}
+		},
+	},
+	"prepareConnectionsFromBoardDef(boarddef)": {
+		"for normal board": {
+			boarddef: {height: 2, width: 3},
+			expected: {
+				1001: {x:1,y:1,nextto:{3: 1002, 4: 2002, 5: 2001}},
+				1002: {x:2,y:1,nextto:{3: 1003, 4: 2003, 5: 2002, 6: 2001, 7: 1001}},
+				1003: {x:3,y:1,nextto:{5: 2003, 6: 2002, 7: 1002}},
+				2001: {x:1,y:2,nextto:{1: 1001, 2: 1002, 3: 2002}},
+				2002: {x:2,y:2,nextto:{1: 1002, 2: 1003, 3: 2003, 7: 2001, 8: 1001}},
+				2003: {x:3,y:2,nextto:{1: 1003, 7: 2002, 8: 1002}}
+			}
+		},
+	},
+	"prepareBoardLayersFromBoardDef(boarddef)": {
+		"for normal call": {
+			"boarddef": {height:2,width:3},
+			"expected": {
+				"LIGHT": {
+					1001: [{COLOUR:"light",X:1,Y:1,POS:1001}],
+					1003: [{COLOUR:"light",X:3,Y:1,POS:1003}],
+					2002: [{COLOUR:"light",X:2,Y:2,POS:2002}]
+				},
+				"DARK": {
+					1002: [{COLOUR:"dark",X:2,Y:1,POS:1002}],
+					2001: [{COLOUR:"dark",X:1,Y:2,POS:2001}],
+					2003: [{COLOUR:"dark",X:3,Y:2,POS:2003}]
+				},
+				"BOARD": {
+					1001: [{COLOUR:"light",X:1,Y:1,POS:1001}],
+					1002: [{COLOUR:"dark",X:2,Y:1,POS:1002}],
+					1003: [{COLOUR:"light",X:3,Y:1,POS:1003}],
+					2001: [{COLOUR:"dark",X:1,Y:2,POS:2001}],
+					2002: [{COLOUR:"light",X:2,Y:2,POS:2002}],
+					2003: [{COLOUR:"dark",X:3,Y:2,POS:2003}]
+				}
+			}
+		}
+	},
+	"addEntitiesFromDef(list,def)": {
+		"for single entity": {
+			list: ["X"],
+			def: {foo:"bar"},
+			expected: ["X",{foo:"bar"}]
+		},
+		"for ALL def": {
+			list: ["X","Y"],
+			def: ["ALL",[666,777],{baz:"bin"}],
+			expected: ["X","Y",{baz:"bin","POS":666},{baz:"bin","POS":777}]
+		}
+	},
+	"prepareEntitiesFromList(list)": {
+		"for a single and an ALL": {
+			list: ["X",["ALL",["Y"],{foo:"bar"}]],
+			expected: ["X",{foo:"bar",POS:"Y"}]
+		}
+	},
+	"prepareState(gamedef,players)": {
+		"for normal call": {
+			gamedef: {
+				setup: "SETUP",
+				board: "BOARD",
+				terrain: {
+					swamp: "SWAMP",
+					marsh: "MARSH"
+				},
+				board: "BOARD"
+			},
+			players: "PLAYERS",
+			context: {
+				prepareEntitiesFromList: {
+					method: function(e){ return "prepped"+e;}
+				},
+				prepareConnectionsFromBoardDef: {
+					returns: "CONNECTIONS",
+					expectedargs: [ ["BOARD"] ]
+				},
+				prepareInitialUnitDataFromSetup: {
+					returns: "UNITS",
+					expectedargs: [ ["SETUP"] ]
+				},
+				prepareBoardLayersFromBoardDef: {
+					returns: {light:"LIGHT",dark:"DARK"},
+					expectedargs: [ ["BOARD"] ]
+				}
+			},
+			expected: {
+				connections: "CONNECTIONS",
+				data: {
+					units: "UNITS"
+				},
+				baselayers: {
+					light: "LIGHT",
+					dark: "DARK",
+					swamp: "preppedSWAMP",
+					marsh: "preppedMARSH"
+				}
+			}
+		}
+	}
+},I);
+
+
+
