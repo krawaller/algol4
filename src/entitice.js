3874d13d35bd5c7849c0bdbdefa33b4b3f00cd51
KungD <david@krawaller.se>
2015-07-16 10:07:41 +0200

    added missing files

---
+++ src/entitice.js
@@ -0,0 +1,93 @@
+(function(){
+var _ = (typeof require !== "undefined" ? require("./lodashmixins") : window._);
+var I = (typeof require !== "undefined" ? require("./immutableextensions.js") : window.Immutable);
+function augmentWithEntiticeFunctions(Algol){
+
+
+// €€€€€€€€€€€€€€€€€€€€€€€€€€€ E N T I T I C E  F U N C T I O N S €€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€*/
+
+/*
+Used in addUnitLayersFromData and ....terrain....
+Will add the entity to correct position in corresponding group layers depending on plr and status
+*/
+Algol.sortEntity = function(layers,entity,groups,plr){
+	var owner = entity.get("owner"),
+		prefix = entity.get("status") === "dead" ? "dead" : "",
+		cat = owner === plr ? "my" : owner === 0 ? "neutral" : "opp",
+		pos = entity.get("pos");
+	return groups.reduce(function(mem,group){
+		return I.pushIn(I.pushIn(mem,[prefix+group,pos],entity),[cat+prefix+group,pos],entity);
+	},layers);
+};
+
+/*
+Called at beginning of every step. Will sort each unit entity from the data object.
+This means either just the "units" group, and also special group if unit has one.
+*/
+Algol.addUnitLayersFromData = function(layers,unitsdata,plr){
+	return unitsdata.reduce(function(mem,unit){
+		var group = unit.get("group");
+		return this.sortEntity(mem,unit,group?["units",group]:["units"],plr);
+	},layers,this);
+};
+
+
+/*
+Used in prepareState
+*/
+Algol.prepareInitialUnitDataFromSetup = function(setup){
+	return I.Map(Algol.prepareEntitiesFromList(setup).reduce(function(map,unit,n){
+		return map.set("unit"+(n+1),unit.set("ID","unit"+(n+1)));
+	},I.Map(),this));
+};
+
+/*
+Helper function used only in prepareEntitiesFromList
+*/
+Algol.addEntitiesFromDef = function(coll,def){
+	if (I.List.isList(def)){ 
+		if (def.first()==="positions"){ // [positions,<list>,<blueprint>]
+			return def.get(1).reduce(function(mem,pos){
+				return mem.push(def.get(2).set("pos",pos));
+			},coll);
+		} else { // [rectangle,topleft,bottomright,blueprint]
+			var blueprint = def.get(3),
+				topleft = parseInt(def.get(1)),
+				bottomright = parseInt(def.get(2));
+			return _.reduce(_.range(Math.floor(topleft/1000),Math.floor(bottomright/1000)+1),function(mem,r){
+				return _.reduce(_.range(topleft % 1000,bottomright % 1000+1),function(mem,c){
+					return mem.push(blueprint.set("pos",r*1000+c));
+				},mem);
+			},coll);
+		}
+	} else {
+		return coll.push(def);
+	}
+}
+
+/*
+Used in prepareInitialUnitDataFromSetup and prepareTerrainLayerFromEntityList
+*/
+Algol.prepareEntitiesFromList = function(deflist){
+	return deflist.reduce(Algol.addEntitiesFromDef,I.List());
+}
+
+/*
+TODO: truncate, or at least fix up
+Used in prepareState.
+*/
+Algol.prepareTerrainLayerFromEntityList = function(list){
+	return Algol.prepareEntitiesFromList(list).reduce(function(mem,e){
+		return I.pushIn(mem,[e.get("pos")],e);
+	},I.Map())
+};
+
+// €€€€€€€€€€€€€€€€€€€€€€€€€ E X P O R T €€€€€€€€€€€€€€€€€€€€€€€€€
+
+} // end augmentWithEntiticeFunctions
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = augmentWithEntiticeFunctions;
+} else {
+    window.augmentWithEntiticeFunctions = augmentWithEntiticeFunctions;
+}})();
---
+++ test/specs/entitice.spec.js
@@ -0,0 +1,163 @@
+/* jshint jasmine: true */
+
+var sinon,jasmineSinon,Algol,_,I,tester;
+if (typeof require === 'function' && typeof module === 'object') {
+	sinon = require('sinon');
+	jasmineSinon = require('jasmine-sinon');
+	Algol = require("../../src/");
+	_ = require("../../src/lodashmixins");
+	I = require("../../src/immutableextensions");
+	tester = require("../tester");
+} else {
+	I = window.Immutable;
+	_ = window._;
+	sinon = window.sinon;
+	tester = window.tester;
+}
+
+tester("The entitice methods",Algol,{
+	"addUnitLayersFromData(layers,unitsdata,currentplr)": {
+		"for normal call": {
+			layers: 1,
+			unitsdata: {
+				id1: {group:"dorks"},
+				id2: {baz:"bin"}
+			},
+			currentplr: "PLR",
+			context: {
+				sortEntity: {
+					method: function(r){ return r+1; },
+					expectedargs: [ [1,{group:"dorks"},["units","dorks"],"PLR"],[2,{baz:"bin"},["units"],"PLR"] ]
+				}
+			},
+			expected: 3
+		}
+	},
+	"sortEntity(layers,entity,groups,currentplr)": {
+		"for unit belonging to currentplr": {
+			layers: {
+				kings: {pos:["foo"]}
+			},
+			entity: {owner: "ME",pos: "pos"},
+			groups: ["kings","units"],
+			currentplr: "ME",
+			expected: {
+				kings: {pos:["foo",{owner: "ME",pos: "pos"}]},
+				mykings: {pos:[{owner: "ME",pos: "pos"}]},
+				units: {pos:[{owner: "ME",pos: "pos"}]},
+				myunits: {pos:[{owner: "ME",pos: "pos"}]}
+			}
+		},
+		"for unit belonging to opponent": {
+			layers: {
+				kings: {pos:["foo"]}
+			},
+			entity: {owner: "OPP",pos: "pos"},
+			groups: ["kings","units"],
+			currentplr: "ME",
+			expected: {
+				kings: {pos:["foo",{owner: "OPP",pos: "pos"}]},
+				oppkings: {pos:[{owner: "OPP",pos: "pos"}]},
+				units: {pos:[{owner: "OPP",pos: "pos"}]},
+				oppunits: {pos:[{owner: "OPP",pos: "pos"}]}
+			}
+		},
+		"for neutral unit": {
+			layers: {
+				kings: {pos:["foo"]}
+			},
+			entity: {owner: 0,pos: "pos"},
+			groups: ["kings","units"],
+			currentplr: "ME",
+			expected: {
+				kings: {pos:["foo",{owner: 0,pos: "pos"}]},
+				neutralkings: {pos:[{owner: 0,pos: "pos"}]},
+				units: {pos:[{owner: 0,pos: "pos"}]},
+				neutralunits: {pos:[{owner: 0,pos: "pos"}]},
+			}
+		},
+		"for dead unit belonging to currentplr": {
+			layers: {
+				deadkings: {pos:["foo"]}
+			},
+			entity: {status: "dead", owner: "ME",pos: "pos"},
+			groups: ["kings","units"],
+			currentplr: "ME",
+			expected: {
+				deadkings: {pos:["foo",{status: "dead", owner: "ME",pos: "pos"}]},
+				mydeadkings: {pos:[{status: "dead", owner: "ME",pos: "pos"}]},
+				deadunits: {pos:[{status: "dead", owner: "ME",pos: "pos"}]},
+				mydeadunits: {pos:[{status: "dead", owner: "ME",pos: "pos"}]}
+			}
+		},
+		"for dead unit belonging to opponent": {
+			layers: {
+				deadkings: {pos:["foo"]}
+			},
+			entity: {status: "dead", owner: "OPP",pos: "pos"},
+			groups: ["kings","units"],
+			currentplr: "ME",
+			expected: {
+				deadkings: {pos:["foo",{status: "dead", owner: "OPP",pos: "pos"}]},
+				oppdeadkings: {pos:[{status: "dead", owner: "OPP",pos: "pos"}]},
+				deadunits: {pos:[{status: "dead", owner: "OPP",pos: "pos"}]},
+				oppdeadunits: {pos:[{status: "dead", owner: "OPP",pos: "pos"}]}
+			}
+		},
+		"for dead neutral unit": {
+			layers: {
+				deadkings: {pos:["foo"]}
+			},
+			entity: {status: "dead", owner: 0, pos: "pos"},
+			groups: ["kings","units"],
+			currentplr: "ME",
+			expected: {
+				deadkings: {pos:["foo",{status: "dead", owner: 0,pos: "pos"}]},
+				neutraldeadkings: {pos:[{status: "dead", owner: 0,pos: "pos"}]},
+				deadunits: {pos:[{status: "dead", owner: 0,pos: "pos"}]},
+				neutraldeadunits: {pos:[{status: "dead", owner: 0,pos: "pos"}]}
+			}
+		}
+	},
+	"prepareInitialUnitDataFromSetup(setup)": {
+		"for normal call": {
+			setup: "SETUP",
+			context: {
+				prepareEntitiesFromList: {
+					returns: [{foo:"bar"},{baz:"bin"}],
+					expectedargs: [["SETUP"]],
+				}
+			},
+			expected: {
+				unit1: {foo:"bar",ID:"unit1"},
+				unit2: {baz:"bin",ID:"unit2"}
+			}
+		},
+	},
+	"addEntitiesFromDef(list,def)": {
+		"for single entity": {
+			list: ["X"],
+			def: {foo:"bar"},
+			expected: ["X",{foo:"bar"}]
+		},
+		"for positions def": {
+			list: ["X","Y"],
+			def: ["positions",[666,777],{baz:"bin"}],
+			expected: ["X","Y",{baz:"bin","pos":666},{baz:"bin","pos":777}]
+		},
+		"for RECTANGLE def": {
+			list: ["X","Y"],
+			def: ["rectangle",3002,4003,{baz:"bin"}],
+			expected: ["X","Y",{baz:"bin","pos":3002},{baz:"bin","pos":3003},{baz:"bin","pos":4002},{baz:"bin","pos":4003}]
+		}
+	},
+	"prepareEntitiesFromList(list)": {
+		"for a single and an positions": {
+			list: ["X",["positions",["Y"],{foo:"bar"}]],
+			expected: ["X",{foo:"bar",pos:"Y"}]
+		}
+	}
+},I);
+
+
+
