165266750b8148be1c9ecc1cfbe9e778574255b6
KungD <david@krawaller.se>
2015-08-16 09:04:28 +0200

    css fix

--- app/games.js
+++ app/games.js
@@ -1,14 +1,14 @@
 module.exports = {
-    archers: I.fromJS(require("../games/archers.json")),
-    krieg: I.fromJS(require("../games/krieg.json")),
-    daggers: I.fromJS(require("../games/daggers.json")),
     amazons: I.fromJS(require("../games/amazons.json")),
-    sombrero: I.fromJS(require("../games/sombrero.json")),
-    conquest: I.fromJS(require("../games/conquest.json")),
-    gogol: I.fromJS(require("../games/gogol.json")),
-    castle: I.fromJS(require("../games/castle.json")),
-    pawnographic: I.fromJS(require("../games/pawnographic.json")),
-    epaminondas: I.fromJS(require("../games/epaminondas.json")),
+    archers: I.fromJS(require("../games/archers.json")),
     breakthru: I.fromJS(require("../games/breakthru.json")),
-    cannon: I.fromJS(require("../games/cannon.json"))
+    cannon: I.fromJS(require("../games/cannon.json")),
+    castle: I.fromJS(require("../games/castle.json")),
+    conquest: I.fromJS(require("../games/conquest.json")),
+    daggers: I.fromJS(require("../games/daggers.json")),
+    epaminondas: I.fromJS(require("../games/epaminondas.json")),
+    gogol: I.fromJS(require("../games/gogol.json")),
+    krieg: I.fromJS(require("../games/krieg.json")),
+    pawnographic: I.fromJS(require("../games/pawnographic.json")),
+    sombrero: I.fromJS(require("../games/sombrero.json"))
 };
